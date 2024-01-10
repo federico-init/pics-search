5cc91946f3c9a67877b78f4f4dd73386ed5c980c
federico-init <fefe.borrometi@gmail.com>
2024-01-10 14:32:47 +0100

    fix: no results case is now properly handled

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,23 @@ import "./App.css";
 
 function App() {
   const [images, setImages] = useState([]);
+  const [hasSearched, setHasSearched] = useState(false);
 
   const handleSubmit = async (searchInput) => {
     const res = await searchImages(searchInput);
 
     setImages(res);
+    setHasSearched(true);
   };
 
   return (
     <div className="App">
       <SearchBar onSubmit={handleSubmit} />
-      <ImageList images={images} />
+      {images.length > 0 ? (
+        <ImageList images={images} />
+      ) : (
+        hasSearched && <p>No images found. Please try another search.</p>
+      )}
     </div>
   );
 }
